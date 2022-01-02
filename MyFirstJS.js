be931e3f2dbe5c27a39677f3031dd11b9f8bd4fe
pavan gowda <pavanggowda28@gmail.com>
2022-01-02 19:12:19 +0530

    FirstCommit

---
+++ MyFirstJS.js
@@ -0,0 +1,17 @@
+console.log(12)
+console.log("My First Pavan Java")
+
+
+let person = {
+
+
+"fname" : "Pavan" ,
+"Lname" : "Gowda"
+
+}
+console.log(typeof person)
+console.log(person)
+
+let studentmarks = [85 , 95 , 52]
+console.log(typeof studentmarks)
+console.log(studentmarks)
---
+++ string.js
@@ -0,0 +1,60 @@
+// let fname = "    My Name Is First       Name       "
+
+// console.log(fname[1]);
+// console.log(fname.includes("My"));
+// console.log(fname.includes("MY"));
+// console.log(fname.startsWith("My"));
+
+// console.log(fname.length);
+// // console.log(fname.split);
+
+// let arr = fname.split(" ");
+
+
+
+// console.log(arr[4]);
+
+
+// console.log(fname.toUpperCase());
+// console.log(fname.toLowerCase());
+
+// console.log(fname.trim());
+
+
+// let pricemessage = "The Total Price is 200  USD";
+// // console.log(typeof pricemessage);
+// let value = pricemessage.split(" ")[4]
+// console.log(typeof value)
+
+// let price = parseInt(value);
+
+// console.log(price);
+
+// if(price == 200){
+
+//     console.log("Pass");
+    
+// }
+     
+//      else{
+
+//             console.log("Fail");
+//         }
+
+
+
+
+    let newmsg = 'Final \tAmount is \'200\' \nUSD' 
+
+    console.log(newmsg);
+   
+    let x = 200
+
+    let MsgDeclared = 'Final Amount is ${x} USD '
+
+    console.log(MsgDeclared);
+
+
+
+
+
