ab0b5fa21be60a697810f63f0616ff9af3aced85
pavan gowda <pavanggowda28@gmail.com>
2022-01-02 19:37:11 +0530

    Arithematic Operator Has Been Pushed

---
+++ OperatorsDemo.js
@@ -0,0 +1,49 @@
+console.log(10/2.5)
+console.log(10/0.0)
+
+
+console.log("5" - 5)
+
+let fnames = 10 + 100 + "Mukhesh Otwani"
+
+console.log(fnames)
+
+console.log(10/true)
+
+let num1 = "10"
+
+console.log(typeof num1)
+
+let num2 = 10
+
+console.log("===========================================")
+
+if(num1 == num2){
+
+    console.log("Matched")
+} else{
+
+    console.log("Fail")
+}
+
+console.log(25>=10)
+console.log(25==100/4)
+console.log(10/100>=0.1 && 10/10==1)
+console.log(10>0 || 10<5 )
+
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+let num3=10
+
+num3 = num3+5
+
+console.log(num3)
+
+num3+=5;
+
+console.log(num3)
+
+num3*=5;
+
+console.log(num3);
+
+
